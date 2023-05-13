10c893fce4423ac3bbd0dd154996b38f5fe7fdf3
unknown <Abbas-khed231>
2023-05-13 21:22:42 +0530

    modified the component checkout and payment

--- ecommerce/src/component/Checkout.js
+++ ecommerce/src/component/Checkout.js
@@ -10,6 +10,24 @@ function Checkout() {
     navigate("/payment");
   }
 
+  function validateForm() {
+    const fullName = document.getElementById("full-name").value;
+    const email = document.getElementById("email").value;
+    const address = document.getElementById("address").value;
+    const city = document.getElementById("city").value;
+    const state = document.getElementById("state").value;
+    const pincode = document.getElementById("pincode").value;
+
+    if (!fullName || !email || !address || !city || !state || !pincode) {
+      alert("Please fill out all fields.");
+      return false;
+    }
+
+    // Add additional validation logic here, such as checking for valid email addresses or zip codes
+
+    return true;
+  }
+
   return (
     <>
       <Navbar/>
@@ -18,33 +36,35 @@ function Checkout() {
       </div>
       <div style={{display:"flex", justifyContent:"space-evenly", alignContent:"center"}}>
         <div>
-          <TextField type="text" label="Full name" style={{ width: "400px", marginTop: "2rem" }} variant="standard" required></TextField>
+          <TextField type="text" id="full-name" label="Full name" style={{ width: "400px", marginTop: "2rem" }} variant="standard" required></TextField>
         </div>
         <div>
-          <TextField type="email" label="Email Address" style={{ width: "400px", marginTop: "2rem" }} variant="standard"required></TextField>
-        </div>
-      </div>
-      <div style={{display:"flex", justifyContent:"space-evenly", alignContent:"center"}}>
-        <div>
-          <TextField type="text" label="Address" style={{ width: "400px", marginTop: "2rem" }} variant="standard" required></TextField>
-        </div>
-        <div>
-          <TextField type="text" label="City" style={{ width: "400px", marginTop: "2rem" }} variant="standard" required></TextField>
+          <TextField type="email" id="email" label="Email Address" style={{ width: "400px", marginTop: "2rem" }} variant="standard" required></TextField>
         </div>
       </div>
       <div style={{display:"flex", justifyContent:"space-evenly", alignContent:"center"}}>
         <div>
-          <TextField type="text" label="State" style={{ width: "400px", marginTop: "2rem" }} variant="standard" required></TextField>
+          <TextField type="text" id="address" label="Address" style={{ width: "400px", marginTop: "2rem" }} variant="standard" required></TextField>
         </div>
         <div>
-          <TextField type="text" label="Pincode" style={{ width: "400px", marginTop: "2rem" }} variant="standard" required></TextField>
+          <TextField type="text" id="city" label="City" style={{ width: "400px", marginTop: "2rem" }} variant="standard" required></TextField>
+        </div>
+      </div>
+      <div style={{display:"flex", justifyContent:"space-evenly", alignContent:"center"}}>
+        <div>
+          <TextField type="text" id="state" label="State" style={{ width: "400px", marginTop: "2rem" }} variant="standard" required></TextField>
+        </div>
+        <div>
+          <TextField type="text" id="pincode" label="Pincode" style={{ width: "400px", marginTop: "2rem" }} variant="standard" required></TextField>
         </div>
       </div>
       <div style={{display:"flex", justifyContent:"space-evenly", alignContent:"center", marginTop:"30px"}}>
-        <Button style={{color:"black", border:" 2px solid black"}} onClick={handleContinue} variant="outlined">Continue to Payment</Button>
+        <Button style={{color:"black", border:" 2px solid black"}} onClick={() => validateForm() && handleContinue()} variant="outlined">Continue to Payment</Button>
       </div>
     </>
   );
 }
 
 export default Checkout;
+
+
--- ecommerce/src/component/Payment.js
+++ ecommerce/src/component/Payment.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
-import {Typography,Button,RadioGroup, Radio,FormControl,FormControlLabel} from "@mui/material";
-
+import { useNavigate } from "react-router";
+import {
+  Typography,
+  Button,
+  RadioGroup,
+  Radio,
+  FormControl,
+  FormControlLabel,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 
 function Payment() {
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState("");
+  const [error, setError] = useState(false);
 
- 
+  const handleConfirm = () => {
+    setOpen(false);
+    navigate("/Home");
+  };
+
+  const handleSubmit = () => {
+    if (selectedValue !== "") {
+      setError(false);
+      setOpen(true);
+    } else {
+      setError(true);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
 
   return (
     <>
@@ -15,27 +46,73 @@ function Payment() {
           Payment
         </Typography>
       </div>
-      <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+      <div
+        style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
         <FormControl>
           <RadioGroup
             aria-labelledby="demo-radio-buttons-group-label"
             name="radio-buttons-group"
+            value={selectedValue}
+            onChange={handleChange}
           >
             <FormControlLabel
-              value=""
+              value="upi"
               control={<Radio />}
               label="UPI Apps / Netbanking"
             />
-            <FormControlLabel value="male" control={<Radio />} label="Cash on delivery"/>
-            <FormControlLabel value="other" control={<Radio />} label="Pay with Debit/Credit/ATM Cards" />
+            <FormControlLabel
+              value="cod"
+              control={<Radio />}
+              label="Cash on delivery"
+            />
+            <FormControlLabel
+              value="cards"
+              control={<Radio />}
+              label="Pay with Debit/Credit/ATM Cards"
+            />
           </RadioGroup>
         </FormControl>
       </div>
-      <div style={{display:"flex", justifyContent:"space-evenly", alignContent:"center", marginTop:"30px"}}>
-        <Button style={{color:"black", border:" 2px solid black"}} variant="outlined">Submit</Button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignContent: "center",
+          marginTop: "30px",
+        }}
+      >
+        <Button
+          style={{ color: "black", border: " 2px solid black" }}
+          variant="outlined"
+          onClick={handleSubmit}
+        >
+          Submit
+        </Button>
       </div>
+      {error && (
+        <Typography
+          variant="body1"
+          color="error"
+          style={{ textAlign: "center", marginTop: "10px" }}
+        >
+          Please select a payment option.
+        </Typography>
+      )}
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Thank you for shopping!</DialogTitle>
+        <DialogContent>
+          <Typography>Payment successful.</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleConfirm}>OK</Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
 
 export default Payment;
+
+
+
